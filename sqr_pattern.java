06c9f5af10190b0e3c1b06158c4b17f01cb94428
priyankamaurya1 <112312030+priyankamaurya1@users.noreply.github.com>
2023-11-29 21:49:25 +0530

    Add files via upload

---
+++ sqr_pattern.java
@@ -0,0 +1,24 @@
+package pattern;
+ import java.util.*;
+
+public class sqr_pattern {
+	public static void main(String[] args) {
+		 Scanner sc = new Scanner(System.in);
+		 int  n= sc.nextInt();
+		 int i=1;
+		 while(i<=n){
+			 int j=1;
+			 while(j<=n){
+				 System.out.print(n);
+				 j++;
+			 }
+			 System.out.println("");
+			 i++;
+		 }
+	
+		
+	}
+
+}
+
+
